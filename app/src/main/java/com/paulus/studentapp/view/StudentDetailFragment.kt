c1e3ebb2fbd1c5472b02bf2d26acfe47001eb48c
raphx <paulus060202@gmail.com>
2024-03-21 14:12:06 +0700

    init

--- app/src/main/java/com/paulus/studentapp/view/StudentDetailFragment.kt
+++ app/src/main/java/com/paulus/studentapp/view/StudentDetailFragment.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.paulus.studentapp.R
 import com.paulus.studentapp.databinding.FragmentStudentDetailBinding
+import com.paulus.studentapp.databinding.FragmentStudentListBinding
+import com.paulus.studentapp.viewmodel.DetailViewModel
+import com.paulus.studentapp.viewmodel.ListViewModel
 
 class StudentDetailFragment : Fragment() {
+    private lateinit var viewModel: DetailViewModel
     private lateinit var binding: FragmentStudentDetailBinding
 
     override fun onCreateView(
@@ -21,6 +27,19 @@ class StudentDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        observeViewModel()
     }
 
+    fun observeViewModel() {
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            binding.txtID.setText(viewModel.studentLD.value?.id)
+            binding.txtName.setText(viewModel.studentLD.value?.name)
+            binding.txtBod.setText(viewModel.studentLD.value?.dob)
+            binding.txtPhone.setText(viewModel.studentLD.value?.phone)
+        })
+    }
 }
